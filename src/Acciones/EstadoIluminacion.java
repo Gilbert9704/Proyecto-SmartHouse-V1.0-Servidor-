a54b53f6e385ed842f9912a4e1a31415e9257dd7
Gilbert Andres Angulo Saucedo <gilbertagulo2014@gmail.com>
2016-05-22 23:02:30 -0500

    Proyecto Final del curso de Programación Orientada a Objetos

---
+++ src/Acciones/EstadoIluminacion.java
@@ -0,0 +1,13 @@
+package Acciones;
+
+public interface EstadoIluminacion {
+	
+	public boolean luzAlcoba1();
+	public boolean luzAlcoba2();
+	public boolean luzLavanderia();
+	public boolean luzBano();
+	public boolean luzEntretenimiento();
+	public boolean luzCocina();
+	public boolean luzSalaComedor();
+	
+}
